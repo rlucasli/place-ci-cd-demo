58a6599537a0b13269f07586c1e16f93dc34871f
Ted Hung <thung@roblox.com>
2024-10-02 16:26:09 -0700

    Bug bash for Publish via Sessions API

--- scripts/lua/create_package_task.lua
+++ scripts/lua/create_package_task.lua
@@ -33,7 +33,7 @@ local function publish_package_asset(instance)
 	local as = game.AssetService
 
 	local requestParameters = {
-		CreatorId = 1253904492,
+		CreatorId = 7385491146,
 		CreatorType = Enum.AssetCreatorType.User,
 		Name = "A Lua Library",
 		Description = "hack your game",
--- scripts/lua/create_version_task.lua
+++ scripts/lua/create_version_task.lua
@@ -42,7 +42,7 @@ local function publish_package_asset(instance)
 	print("creating a new version")
 	local versionId
 	local success, err = pcall(function()
-		result, versionId = as:CreateAssetVersionAsync(instance, Enum.AssetType.Model, 129883978505859, requestParameters)
+		result, versionId = as:CreateAssetVersionAsync(instance, Enum.AssetType.Model, 109578110827226, requestParameters)
 	end)
 
 	if success then
