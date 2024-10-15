3640d1991aef8037905911a6d7505d86cd87fd77
Jeff Witthuhn <183666970+jwitthuhn-roblox@users.noreply.github.com>
2024-10-15 15:48:31 -0700

    Test commit

--- scripts/lua/create_version_task.lua
+++ scripts/lua/create_version_task.lua
@@ -33,7 +33,7 @@ local function publish_package_asset(instance)
 	local as = game.AssetService
 
 	local requestParameters = {
-		CreatorId = 1253904492,
+		CreatorId = 1918119960,
 		CreatorType = Enum.AssetCreatorType.User,
 		Name = "A Lua Library",
 		Description = "good library",
@@ -42,7 +42,7 @@ local function publish_package_asset(instance)
 	print("creating a new version")
 	local result, versionId
 	local success, err = pcall(function()
-		result, versionId = as:CreateAssetVersionAsync(instance, Enum.AssetType.Model, 124954374814947, requestParameters)
+		result, versionId = as:CreateAssetVersionAsync(instance, Enum.AssetType.Model, 104728170863560, requestParameters)
 	end)
 
 	if success then
